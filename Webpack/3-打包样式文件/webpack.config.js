5e0392a75e79b41aeefbade517c7ba4a142dac5e
Anakinliu <gugeliuyinquan@gmail.com>
2021-06-03 21:47:55 +0800

    太多了!!!

---
+++ Webpack/3-打包样式文件/webpack.config.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * 此配置文件指示Webpack干什么活
+ * 
+ * 构建共具都是基于NodeJS平台的,注意与项目本身区分开来.
+ * NodeJSm默认用CommonJS
+ */
+const path = require('path')
+
+module.exports = {
+    enrty: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__filename, 'bundle')
+    },
+
+    // loder配置
+    module: {
+        rules: [
+            // 详细的loader配置
+            {
+                test: /\.css$/,  // 正则表达式,匹配.css结尾的文件
+                // use中的执行顺序是从后到前,先'css-loader',后'style-loader'
+                use: [
+                    // 创建style标签, 将JS中的样式资源插入head中
+                    'style-loader',
+                    // 将CSS文件变为commonjs模块加载到js中,里面内容是样式的字符串
+                    'css-loader'
+                ]
+            }  
+        ]
+    },
+
+    // plugins 配置
+    plugins: {
+
+    }
+}
